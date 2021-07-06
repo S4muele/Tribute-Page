dcbd4b900cb73a49430aebf916834cf27d812e92
Samuele <anthony.sime94@gmail.com>
2021-07-06 16:07:41 +0200

    Enhanced Website Responsiveness

--- script.js
+++ script.js
@@ -33,22 +33,3 @@ function nextSlide() {
 
 document.getElementById("carousel-button-prev").addEventListener("click", prevSlide)
 document.getElementById("carousel-button-next").addEventListener("click", nextSlide)
-
-const imgCaptionEl = document.getElementById("img-caption")
-const imgEl = document.getElementById("image")
-const preBtnEl = document.getElementById("carousel-button-prev")
-const nextBtnEl = document.getElementById("carousel-button-next")
-
-function addBoxShadow() {
-    imgEl.classList.add("box-shadow")
-}
-function removeBoxShadow() {
-    imgEl.classList.remove("box-shadow")
-}
-
-imgCaptionEl.addEventListener("mouseover", addBoxShadow)
-imgCaptionEl.addEventListener("mouseout", removeBoxShadow)
-preBtnEl.addEventListener("mouseover", addBoxShadow)
-preBtnEl.addEventListener("mouseout", removeBoxShadow)
-nextBtnEl.addEventListener("mouseover", addBoxShadow)
-nextBtnEl.addEventListener("mouseout", removeBoxShadow)
